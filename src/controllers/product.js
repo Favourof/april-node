5a90435755ff5a3bf1df561c91289da1e459580a
ezekiel <favourtobiloba200@gmail.com>
2026-05-13 04:38:22 +0100

    auth and product crud completion

--- src/controllers/product.js
+++ src/controllers/product.js
@@ -41,4 +41,85 @@ const getAllProduct = async (req, res) => {
   }
 };
 
-module.exports = { createProduct, getAllProduct };
+const getSingleProduct = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findById(id);
+
+    if (!product) {
+      return res
+        .status(404)
+        .json({ status: false, message: "Product not Found" });
+    }
+
+    // const productDTO = {
+    //   title: product.title,
+    //   price: product.price,
+    // };
+
+    return res.status(200).json({
+      status: true,
+      message: "Product fetch Successfully",
+      // product: productDTO,
+      product,
+    });
+  } catch (error) {
+    console.log(error.message);
+    return res.status(500).json({ status: false, message: error.message });
+  }
+};
+
+const deleteProduct = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findByIdAndDelete(id);
+
+    if (!product) {
+      return res
+        .status(404)
+        .json({ status: false, message: "Product not Found" });
+    }
+
+    return res.status(200).json({
+      status: true,
+      message: "Product Deleted Successfully",
+    });
+  } catch (error) {
+    console.log(error.message);
+    return res.status(500).json({ status: false, message: error.message });
+  }
+};
+
+const updateProduct = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const product = await Product.findByIdAndUpdate(id, req.body, {
+      returnDocument: "after",
+    });
+
+    if (!product) {
+      return res
+        .status(404)
+        .json({ status: false, message: "Product not Found" });
+    }
+
+    return res.status(200).json({
+      status: true,
+      message: "Product Updated Successfully",
+      // product: productDTO,
+      product,
+    });
+  } catch (error) {
+    console.log(error.message);
+    return res.status(500).json({ status: false, message: error.message });
+  }
+};
+
+module.exports = {
+  createProduct,
+  getAllProduct,
+  getSingleProduct,
+  deleteProduct,
+  updateProduct,
+};
--- src/routes/product.js
+++ src/routes/product.js
@@ -1,8 +1,17 @@
 const express = require("express");
-const { createProduct, getAllProduct } = require("../controllers/product");
+const {
+  createProduct,
+  getAllProduct,
+  getSingleProduct,
+  deleteProduct,
+  updateProduct,
+} = require("../controllers/product");
 
 const route = express.Router();
 
 route.post("/", createProduct);
 route.get("/", getAllProduct);
+route.post("/:id", getSingleProduct);
+route.delete("/:id", deleteProduct);
+route.put("/:id", updateProduct);
 module.exports = route;
